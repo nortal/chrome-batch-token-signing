94596fc8e5fcdea3e5a0c8313a406220e31fc826
allan.juhanson <allan.juhanson@nortal.com>
2016-04-12 15:17:24 +0300

    SKEID-37 Lithuanian giesekse usb token support via pkcs11
    # Conflicts:
    #	host-windows/host-windows.vcxproj.filters
    #	host-windows/resource.h

--- host-windows/RequestHandler.cpp
+++ host-windows/RequestHandler.cpp
@@ -19,7 +19,7 @@
 #include "RequestHandler.h"
 #include "Logger.h"
 #include "VersionInfo.h"
-#include "CertificateSelection.h"
+#include "CertificateSelectorFactory.h"
 #include "ContextMaintainer.h"
 #include "SignerFactory.h"
 #include "ProgressBar.h"
@@ -119,7 +119,8 @@ void RequestHandler::handleVersionRequest() {
 
 void RequestHandler::handleCertRequest() {
 	validateSecureOrigin();
-	string selectedCert = CertificateSelection::getCert();
+	CertificateSelector * certificateSelector = CertificateSelectorFactory::createCertificateSelector();
+	string selectedCert = certificateSelector->getCert();
 	ContextMaintainer::saveCertificate(selectedCert);
 	jsonResponse << "cert" << selectedCert;
 }
